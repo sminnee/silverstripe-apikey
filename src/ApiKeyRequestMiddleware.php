812d4106c53027329cfdbd87b174106bf6ccee1b
Matt Lang <matt.lang.42@gmail.com>
2024-10-09 09:47:58 +1000

    fix using deprecated/removed login method

--- src/ApiKeyRequestMiddleware.php
+++ src/ApiKeyRequestMiddleware.php
@@ -6,7 +6,9 @@ use LogicException;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Control\HTTPResponse_Exception;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Security\IdentityStore;
 
 /**
  * Initialises the versioned stage when a request is made.
@@ -30,7 +32,7 @@ class ApiKeyRequestMiddleware implements HTTPMiddleware
                 throw new HTTPResponse_Exception("Bad X-API-Key", 400);
             }
 
-            $matchingKey->Member()->logIn();
+            Injector::inst()->get(IdentityStore::class)->logIn($matchingKey->Member());
 
             $matchingKey->markUsed();
         }

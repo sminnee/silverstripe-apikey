f3710dbd748e3691a7dfd9e21216c1ccee81eeb4
Matt Lang <matt.lang.42@gmail.com>
2024-10-08 11:46:18 +1000

    fix tests for SS5

--- tests/unit/GraphQL/ApiKeyAuthenticatorTest.php
+++ tests/unit/GraphQL/ApiKeyAuthenticatorTest.php
@@ -45,7 +45,7 @@ class ApiKeyAuthenticatorTest extends SapphireTest
         ],
     ];
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->member = $this->objFromFixture(Member::class, 'admin');
